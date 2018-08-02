b8c1fab04950206a802053cc2c85a29f33f16115
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2018-08-02 22:41:35 +0300

    Load words into a constant ALL_WORDS

--- typo/constants.py
+++ typo/constants.py
@@ -2,3 +2,9 @@ SCENE_START = 'SCENE_START'
 SCENE_GAME = 'SCENE_GAME'
 
 WORD_COUNT = 5
+
+def load_words(source):
+    with open(source, 'r') as f:
+        return f.read().split()
+
+ALL_WORDS = load_words('words.txt')
--- typo/reducer.py
+++ typo/reducer.py
@@ -8,17 +8,13 @@ import constants
 def initial_state():
     return {
         'current_scene': constants.SCENE_START,
-        'all_words': load_words('words.txt'),
+        'all_words': constants.ALL_WORDS,
         'start_time': None,
         'end_time': None,
         'current_word': '',
         'position': 0,
         'count': 0,
     }
-
-def load_words(source):
-    with open(source, 'r') as f:
-        return f.read().split()
 
 def typo_reducer(state, action_type, **kwargs):
     if action_type == actions.START_GAME:
