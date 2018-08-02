5128b0ae9fe83235f02568d89714e521a8bd48b8
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2018-08-02 20:03:07 +0300

    Implement 'typo' word matching

--- typo/typo.py
+++ typo/typo.py
@@ -1,16 +1,42 @@
+from types import SimpleNamespace
 import pyxel
 
-pyxel.init(
-    width=160,
-    height=120,
-    caption='typo',
-)
+class Game:
+    def __init__(self):
+        pyxel.init(160, 120)
 
-def update():
-    if pyxel.btnp(pyxel.KEY_Q):
-        pyxel.quit()
+        self.state = SimpleNamespace(
+            remaining_input='typo',
+            words_typed=1
+        )
 
-def draw():
-    pyxel.cls(0)
+    def run(self):
+        pyxel.run(self.update, self.draw)
 
-pyxel.run(update, draw)
+    def update(self):
+        if self.state.remaining_input:
+            for character, key in self.character_map.items():
+                if self.state.remaining_input[0] == character and pyxel.btnp(key):
+                    self.state.remaining_input = self.state.remaining_input[1:]
+                    break
+
+        else:
+            self.state.words_typed += 1
+            self.state.remaining_input = 'typo'
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.text(10, 10, str(self.state.remaining_input), 7)
+
+    @property
+    def character_map(self):
+        return {
+            'o': pyxel.KEY_O,
+            'p': pyxel.KEY_P,
+            't': pyxel.KEY_T,
+            'y': pyxel.KEY_Y,
+        }
+
+
+if __name__ == '__main__':
+    Game().run()
