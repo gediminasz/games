93a97f8383ab37ee76c80aabc1f1984b9a2efeab
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2018-08-04 12:24:32 +0300

    Move load_words to words module

--- typo/constants.py
+++ typo/constants.py
@@ -1,10 +1,8 @@
+from words import load_words
+
 SCENE_START = 'SCENE_START'
 SCENE_GAME = 'SCENE_GAME'
 
 WORD_COUNT = 10
-
-def load_words(source):
-    with open(source, 'r') as f:
-        return f.read().split()
 
 ALL_WORDS = load_words('words.txt')
--- typo/words.py
+++ typo/words.py
@@ -2,5 +2,9 @@ from random import choice
 
 import constants
 
+def load_words(source):
+    with open(source, 'r') as f:
+        return f.read().split()
+
 def next_word():
     return choice(constants.ALL_WORDS)
