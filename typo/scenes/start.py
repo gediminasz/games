5d124f7cf7b5663b575f078c0d8d6324a7d8e1a9
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2018-08-04 11:26:17 +0300

    Add ability to quit the game by pressing Q

--- typo/scenes/start.py
+++ typo/scenes/start.py
@@ -9,10 +9,13 @@ class StartScene:
     def update(self, state, dispatch):
         if pyxel.btnp(pyxel.KEY_SPACE):
             dispatch(actions.START_GAME, time=time.time())
+        elif pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
 
     def draw(self, state):
         pyxel.text(70, 40, 'TYPO', 3)
         pyxel.text(35, 60, 'Press <space> to start', 7)
+        pyxel.text(55, 70, '<Q> to quit', 7)
 
         if state['wpm']:
             pyxel.text(45, 80, f"Last WPM: {state['wpm']:.2f}", 7)
