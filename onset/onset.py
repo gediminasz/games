0e241d561320b3548aedba3fd4718151a41dce97
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2018-08-16 16:50:12 +0300

    Add ACTIVATE_FRETS action
    And some code clean up

--- onset/onset.py
+++ onset/onset.py
@@ -17,6 +17,16 @@ AUDIO_FILE = sys.argv[1]
 FRETS_POSITION = 40, 100
 FRET_SIZE = 16, 8
 
+DRAW_SECONDS_AHEAD = 2
+SPEED = 60
+NOTE_WINDOW = 0.1  # second
+
+ASSETS_FRET_INACTIVE = 0
+ASSETS_FRET_ACTIVE = 1
+ASSETS_FRET_HIT = 2
+ASSETS_NOTE = 3
+ASSETS_NOTE_SMALL = 4
+
 
 class Game:
     def __init__(self):
@@ -30,8 +40,6 @@ class Game:
 
         self.next_tab = 0
         self.fret = None
-        self.fret_hit = False
-        self.active_frets = (False,) * 5
 
     def run(self):
         pyxel.run(self.update, self.draw)
@@ -44,31 +52,25 @@ class Game:
         else:
             if self.time > self.tabs[self.next_tab]['time']:
                 self.fret = tabs.fret(self.tabs[self.next_tab]['strength'])
-                self.fret_hit = False
                 self.next_tab = min(self.next_tab + 1, len(self.tabs) - 1)
 
-        self.active_frets = [
-            0 if pyxel.btn(key) else 1
-            for key in (pyxel.KEY_1, pyxel.KEY_2, pyxel.KEY_3, pyxel.KEY_4, pyxel.KEY_5)
-        ]
+        if any(map(pyxel.btnp, self.fret_keys)) or any(map(pyxel.btnr, self.fret_keys)):
+            self.store.dispatch(
+                actions.ACTIVATE_FRETS,
+                frets=[pyxel.btn(key) for key in self.fret_keys]
+            )
 
         if pyxel.btnp(pyxel.KEY_SPACE):
-            if self.tabs[self.next_tab]['time'] - self.time < 0.1:
+            if self.note_hit:
                 print('HIT')
-                if self.active_frets[self.fret] == 0:  # pressed
-                    self.active_frets[self.fret] = 2  # hit
-                    self.fret_hit = True
             else:
                 print('MISS')
 
     def draw(self):
         pyxel.cls(0)
 
-        for i, _ in enumerate(tabs.FRETS):
-            self.draw_fret(i, 1)
-
-        for i, state in enumerate(self.active_frets):
-            self.draw_fret(i, state)
+        for fret, active in enumerate(self.store.state['active_frets']):
+            self.draw_fret(fret, ASSETS_FRET_ACTIVE if active else ASSETS_FRET_INACTIVE)
 
         self.draw_incoming_tabs()
 
@@ -79,13 +81,13 @@ class Game:
     def draw_incoming_tabs(self):
         incoming_tabs = [
             tab for tab in self.tabs[self.next_tab:]
-            if tab['time'] < self.time + 2  # 2 seconds ahead
+            if tab['time'] < self.time + DRAW_SECONDS_AHEAD
         ]
 
         _, y = FRETS_POSITION
         for tab in incoming_tabs:
             i = tabs.fret(tab['strength'])
-            self.draw_tab(i, y - int((tab['time'] - self.time) * 60), 3)
+            self.draw_tab(i, y - int((tab['time'] - self.time) * SPEED), ASSETS_NOTE)
 
     def draw_tab(self, i, y, state):
         x, _ = FRETS_POSITION
@@ -95,6 +97,17 @@ class Game:
     @property
     def time(self):
         return time() - self.store.state['start_time']
+
+    @property
+    def fret_keys(self):
+        return (pyxel.KEY_1, pyxel.KEY_2, pyxel.KEY_3, pyxel.KEY_4, pyxel.KEY_5)
+
+    @property
+    def note_hit(self):
+        return (
+            (self.tabs[self.next_tab]['time'] - self.time < NOTE_WINDOW) and
+            self.store.state['active_frets'][self.fret]
+        )
 
 
 if __name__ == '__main__':
--- onset/reducer.py
+++ onset/reducer.py
@@ -1,10 +1,12 @@
 import actions
 
+INITIAL_ACTIVE_FRETS = (False,) * 5
 
 def initial_state():
     return {
         'playing': False,
         'start_time': None,
+        'active_frets': INITIAL_ACTIVE_FRETS,
     }
 
 
@@ -14,6 +16,10 @@ def reducer(state, action_type, **kwargs):
             **state,
             'playing': True,
             'start_time': kwargs['time'],
+            'active_frets': INITIAL_ACTIVE_FRETS,
         }
+
+    if action_type == actions.ACTIVATE_FRETS:
+        return {**state, 'active_frets': kwargs['frets']}
 
     return state
